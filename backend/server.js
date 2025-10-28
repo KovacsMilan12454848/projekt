69a67b8f5367aef0d6b899d35fa6f08b1b90f6bd
Alex <domokosalex01@gmail.com>
2025-10-28 16:41:04 +0100

    Server elso lepesek, kerdoiv javitas

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glicin'
+});
+
+
+//add
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Szerver fut a http://localhost:3000 címen');
+});
--- frontend/js/kerdoiv.js
+++ frontend/js/kerdoiv.js
@@ -1,98 +1,67 @@
-class Kerdoiv {
-    constructor() {
-        this.kerdesek = [];
-        //this.Kitoltesek = [];
-
+class Kerdes {
+    constructor(sorszam, szoveg, valaszok) {
+        this.sorszam = sorszam;
+        this.szoveg = szoveg;
+        this.valaszok = valaszok; 
+        this.megcsinalta = false; 
     }
-
-    Felpakolas(Id, kerdes, valasz, megcsinalta) {
-        const adat = { Id, kerdes, valasz, megcsinalta };
-        this.kerdesek.push(adat);
-    }
-
-   /* Kitoltotes(kitoltot) {
-        this.Kitoltesek.push(kitoltot)
-
-
-    }*/
-
 }
 
-const kerd = new Kerdoiv();
+
+const kerdesek = [
+    new Kerdes(1, "Ismeri már az L-glicinről?", ["Igen","Nem","Meglehet"]),
+    new Kerdes(2, "Mi a korosztáya?", ["15-25","26-35","36-50","50+"]),
+    new Kerdes(3, "Egészségügyi állapota?", ["Egészséges","általában betegeskedem","Tartós betegségem van"])
+];
 
 
+let index = 0;
+const kitoltes_eredmeny = {}; 
 
-kerd.Felpakolas(1, "Ismeri már az L-glicinről?", ["Igen", "Nem", "Meglehet"], false)
-kerd.Felpakolas(2, "Mi a korosztáya?", ["15-25", "26-35", "36-50","50+"], false)
-kerd.Felpakolas(3, "Egészségügyi állapota?", ["Egészséges","általában betegeskedem", "Tartós betegségem van"], false)
-kerd.Felpakolas(4, "Milyen nemzetiségü?", ["Magyar", "Kisebbség", "Egyéb"], false)
-kerd.Felpakolas(5, "Milyen településen lakik?", ["Nagyváros", "Város", "Falu"], false)
-kerd.Felpakolas(6, "Honnan halott az L-glicinről?", ["Családzól", "Ismerősöktől", "Közöségi médiaban","Orvostól"], false)
-kerd.Felpakolas(7, "Milyen végzettséggel rendelkezik?", ["Éretségivel", "Egyetemi", "általános"], false)
-kerd.Felpakolas(8, "Szed-e bármilyen gyógyszert állandó szinten", ["Igen, szedek", "Nem, nemszedek "], false)
-kerd.Felpakolas(9, "Mi a neme?", ["Férfi", "Nő", "Nem akarom megadni"], false)
 
-var index = 1;
-
-window.addEventListener('load', function () {
+window.addEventListener("load", function(){{
     Betoltes(index);
-});
+}})
 
 
 function Betoltes(i) {
-
     const oldal = document.getElementById("Kerdesek");
-    const adat = {}
-    for (const x of kerd.kerdesek) {
-        if (x.Id == i) {
-            for (var kulcs in x) {
-                if (x.hasOwnProperty(kulcs)) {
-                    adat[kulcs] = x[kulcs];
-                }
-            }
-            break;
-        }
-    }
-
-
+    const adat = kerdesek[i];
 
     oldal.innerHTML = `
-    <h1 class="mt-4 p3 text-center" >${adat.kerdes} </h1>
-    <hr>
-    <form id="Form" class="p-3 g-3"> </form>`
+        <h1 class="mt-4 p3 text-center">${adat.szoveg}</h1>
+        <hr>
+        <form id="Form" class="p-3 g-3"></form>
+    `;
 
     Valasz_lehetosegek(adat);
 
-    let szoveg = ""
-    if (index == kerd.kerdesek.length) { szoveg = "Befejezés" }
-    else { szoveg = "Következő" }
+    const szoveg = (i === kerdesek.length - 1) ? "Befejezés" : "Következő";
 
     oldal.innerHTML += `
-    <div class="text-end p-3 ">
-        <button type="button" class="btn btn-secondary btn-lg" id="btn" onclick="Elozo()">Előző</button>
-        <button type="button"  class="btn btn-success btn-lg"  id="kov" onclick="Kovetkezo()">${szoveg}</button>
-    </div>
-    <div class="   text-center " id="helyzet"></div> `;
-
-
+        <div class="text-end p-3">
+            <button type="button" class="btn btn-secondary btn-lg" onclick="Elozo()">Előző</button>
+            <button type="button" class="btn btn-success btn-lg" onclick="Kovetkezo()">${szoveg}</button>
+        </div>
+        <div class="text-center" id="helyzet"></div>
+    `;
 
     Gombok();
 }
 
-function Valasz_lehetosegek(valaszok) {
+function Valasz_lehetosegek(adat) {
     const ker = document.getElementById("Form");
     ker.innerHTML = "";
-    let lep = 1;
-    for (const val of valaszok.valasz) {
+    let lep = 0;
+    for (const val of adat.valaszok) {
         ker.innerHTML += `<label class="h3">
-        <input type="radio" name="valasz" value="${lep}"   class="big-radio">${val}</label><br>`;
+            <input type="radio" name="valasz" value="${lep}" class="big-radio"> ${val}
+        </label><br>`;
         lep++;
     }
-
 }
 
 function ugras(ugras) {
-
     index = ugras;
     Betoltes(index);
 }
@@ -100,105 +69,57 @@ function ugras(ugras) {
 function Gombok() {
     const helyzet = document.getElementById("helyzet");
     helyzet.innerHTML = "";
-    for (const x of kerd.kerdesek) {
-        if (x.Id == index) {
-            helyzet.innerHTML += `<button class="btn m-1 p-2  col-1 btn-success" onclick="ugras(${x.Id})">${x.Id}</button>`;
-        } else if (x.megcsinalta) {
-            helyzet.innerHTML += `<button class="btn m-1 p-2  col-1 btn-secondary" onclick="ugras(${x.Id})">${x.Id}</button>`;
-        } else {
-            helyzet.innerHTML += `<button class="btn m-1 p-2  col-1 btn-warning" onclick="ugras(${x.Id})">${x.Id}</button>`;
-        }
-
-    }
-
-
+    kerdesek.forEach(k => {
+        const cls = (k.sorszam - 1 === index) ? "btn-success" : (k.megcsinalta ? "btn-secondary" : "btn-warning");
+        helyzet.innerHTML += `<button class="btn m-1 p-2 col-1 ${cls}" onclick="ugras(${k.sorszam - 1})">${k.sorszam}</button>`;
+    });
 }
-
-var kitoltes_eredmeny = [];
 
 function Kovetkezo() {
     const selected = document.querySelector('input[name="valasz"]:checked');
-    const valasz = selected ? selected.value : 0;
+    if (!selected) return alert("Jelöljön meg valamelyik választ!");
 
-   
-     if (index == kerd.kerdesek.length) {
+    const valIndex = parseInt(selected.value);
+    const adat = kerdesek[index];
 
-        if ((valasz > 0 || kerd.kerdesek[index - 1].megcsinalta)) {
+    adat.megcsinalta = true;
+    kitoltes_eredmeny[adat.sorszam] = adat.valaszok[valIndex];
 
-            kerd.kerdesek[index - 1].megcsinalta = true
-          //  kitoltes_eredmeny.push({ kerdes: index, valasz: valasz })
-            Befejezes();
-        }
-        else {
-            alert("jelöljön meg valamelyik választ")
-        }
-    }
-    else if (index < kerd.kerdesek.length && (valasz > 0 || kerd.kerdesek[index-1].megcsinalta)) {
-
-        kerd.kerdesek[index - 1].megcsinalta = true
-        //kitoltes_eredmeny.push({ kerdes: index, valasz: valasz })
-        index += 1
-        Betoltes(index)
-    }
-    else {
-        alert("jelöljön meg valamelyik választ")
-    }
-
+    if (index < kerdesek.length - 1 && Object.keys(kitoltes_eredmeny).length <= kerdesek.length - 1) {
+    index++;
+    Betoltes(index);
+} else {
+    Befejezes();
 }
-
-
-function Befejezes() {
-
-    const oldal = document.getElementById("Kerdesek");
-    const befejezett = kerd.kerdesek.every(n => n.megcsinalta);
-    if (!befejezett) { alert("Kérem töltse ki a tesztet, mert igy nem lehet beküldeni!") }
-    else {
-
-       // kerd.Kitoltotes(kitoltes_eredmeny)
-       // kitoltes_eredmeny = {}
-        oldal.innerHTML = `  
-    <h1 class="mt-4 p3 text-center"> Köszönjük hogy kitöltötte a tesztet </h1>
-     <br>
-    <div class="container">
-        <div class="row">
-            <div class="col-6 ">
-                <h4>Ha szeretne átmehet az ajánlás oldalra</h4>
-                <a href="#"><button class="btn btn-primary btn-lg">Ajánlás</button></a>
-            </div>
-            <div class="col-6">
-                <h4>Vagy Viszatérhet a főoldalra</h4>
-                <a href="#"><button class="btn btn-primary btn-lg">Kérdőív</button></a>
-            </div>
-            
-        </div>
-    </div>`
-    }
-/*
-    <div class="col-12">
-                <h4>kitöltés eredmény</h4>
-                <button class="btn btn-primary btn-lg " onclick="Eredmeny()">Eredmény</button>
-                
-            </div>*/
-
 }
 
 function Elozo() {
-
-    if (index == 1) {
-        alert("Ettől viszább nem lehet menni!")
-    }
-    else if (index <= kerd.kerdesek.length) {
-        index -= 1
-        Betoltes(index)
-    }
-
+    if (index === 0) return alert("Ettől visszább nem lehet menni!");
+    index--;
+    Betoltes(index);
 }
 
-/*
-function Eredmeny() {
+function Befejezes() {
+    console.log("Kitöltés eredménye:", kitoltes_eredmeny);
+
+
+    
     const oldal = document.getElementById("Kerdesek");
-
-    oldal.innerHTML = `<h1> Ennyien töltötték ki ${kerd.Kitoltesek.length}`
+    oldal.innerHTML = `
+        <h1 class="mt-4 p3 text-center">Köszönjük, hogy kitöltötte a tesztet!</h1>
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h4>Ha szeretne, átmehet az ajánlás oldalra</h4>
+                    <a href="#"><button class="btn btn-primary btn-lg">Ajánlás</button></a>
+                </div>
+                <div class="col-6">
+                    <h4>Vagy visszatérhet a főoldalra</h4>
+                    <a href="../html/index.html"><button class="btn btn-primary btn-lg">Főoldal</button></a>
+                </div>
+            </div>
+        </div>
+    `;
 
 }
-    */
