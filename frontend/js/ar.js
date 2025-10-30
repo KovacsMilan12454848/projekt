1e2572d3d18d24dac5ee4b82a3c2c98d73d5ec4c
Alex <domokosalex01@gmail.com>
2025-10-30 13:56:07 +0100

    Kivettem lorem szoveget, design valtoztatasok

--- frontend/js/ar.js
+++ frontend/js/ar.js
@@ -2,27 +2,33 @@ const ctx = document.getElementById('ar').getContext('2d');
 const chart = new Chart(ctx, {
     type: 'bar', 
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green'], 
+        labels: ['L-Theanine (100 mg, 60 kapszula)', 'Glicin (500g por)','N-Acetylcysteine (NAC) (750 mg, 120 kapszula)'], 
         datasets: [{
             label: '# of Votes',
-            data: [5, 2, 3, 5], 
+            data: [22.44, 14.99, 21.49, ], 
             backgroundColor: [
-                'red', 'blue', 'yellow', 'green'
-            ],
-            borderColor: [
-                'darkred', 'darkblue', 'goldenrod', 'darkgreen'
+                'red', 'green', 'yellow'
             ],
             borderWidth: 1
         }]
     },
     options: {
-        responsive: true,
+        responsive: true, 
 
         scales: {
             y: {
                 beginAtZero: true 
             }
+        },
+        plugins: {
+      tooltip: {
+        callbacks: {
+          label: function(context) {
+            return '€' + context.parsed.y; // show only the value in euros
+          }
         }
+      }
+    }
     }
 });
 
